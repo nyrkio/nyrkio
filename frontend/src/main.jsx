9444ed92ae8a20b2b469447e0fd04ef9358e5dc6
Matt Fleming <matt@nyrk.io>
2024-01-19 20:25:13 +0000

    frontend: Delete unused files and remove commented out code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,26 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import Root, { loader as rootLoader } from "./routes/root.jsx";
-// import ErrorPage from "./routes/error-page.jsx";
-// import Login from "./routes/login.jsx";
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//     errorElement: <ErrorPage />,
-//     loader: rootLoader,
-//     children: [
-//       {
-//         path: "/login",
-//         element: <Login />,
-//       },
-//     ],
-//   },
-// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
