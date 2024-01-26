5b34cec05fd5978bc985f15619d91cde53e5608b
Matt Fleming <matt@nyrk.io>
2024-01-26 11:56:11 +0000

    fe/nav: Add link to front page in navbar
    Also remove other links that won't have content for a while.

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -18,8 +18,8 @@ const NavigationItems = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Company
+            <a className="nav-link" aria-current="page" href="/">
+              Nyrkiö
             </a>
           </li>
           <li className="nav-item">
@@ -30,16 +30,6 @@ const NavigationItems = () => {
           <li className="nav-item">
             <a className="nav-link" href="#">
               Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Services
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Research
             </a>
           </li>
         </ul>
