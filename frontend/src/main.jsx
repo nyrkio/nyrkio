158f8c551531ce413d099b1d886b9cebbf5ec008
Matt Fleming <matt@nyrk.io>
2024-01-27 09:20:09 +0000

    analytics: Send events to posthog

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import posthog from "posthog-js";
+
+posthog.init("phc_pj5OcqAS68hS0aIO0a3lqodk6lRekPEsz3VMdz6o7Z1", {
+  api_host: "https://eu.posthog.com",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
