e2e196300476792a47d2104ff4bc6c9b31b97648
Henrik Ingo <henrik@nyrk.io>
2024-02-08 23:21:55 +0200

    Fix the white gap below the footer (#70)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://nyrk.io",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
