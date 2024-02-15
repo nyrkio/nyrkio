de007363457d46f73ef56df53dc1c1f346e5191a
Matt Fleming <matt@nyrk.io>
2024-02-15 10:45:50 +0000

    benches: Add initial benchmark that queries the default data
    The tests are simple and don't require any credentials to access Nyrkiö.
    But they lay the foundation for more complex tests later.
    
    Also add a script to parse the pytest-benchmark results and submit them
    to https://nyrk.io.

---
+++ backend/benches/process_results.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+import json
+import requests
+import os
+import sys
+
+
+def calculate_unit(value):
+    """
+    Calculate the best unit for the value.
+    """
+    unit = "s"
+    if value < 1.0:
+        value = value * 1000
+        unit = "ms"
+
+    if value < 1.0:
+        value = value * 1000
+        unit = "us"
+
+    # Round to 3 decimal places
+    return round(value, 3), unit
+
+
+def create_nyrkio_payload(commit_info, benchmark):
+    # convert date to epoch
+    timestamp = int(
+        datetime.strptime(commit_info["time"], "%Y-%m-%dT%H:%M:%S%z").timestamp()
+    )
+
+    metrics = []
+    for m in ("median", "mean", "max", "min", "stddev", "iqr"):
+        value, unit = calculate_unit(benchmark["stats"][m])
+        metrics.append({"name": m, "value": value, "unit": unit})
+
+    return {
+        "timestamp": timestamp,
+        "metrics": metrics,
+        "attributes": {
+            "git_commit": [commit_info["id"]],
+            "branch": [commit_info["branch"]],
+            "git_repo": ["https://github.com/nyrkio/nyrkio"],
+        },
+    }
+
+
+def submit_results(test_name, results, token):
+    # Submit results
+    response = requests.post(
+        f"https://nyrk.io/api/v0/result/{test_name}",
+        json=results,
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        },
+    )
+    response.raise_for_status()
+
+
+def main(filename):
+    # ...
+    # Load the results from the file
+    with open(filename, "r") as f:
+        results = json.load(f)
+
+    username = os.environ.get("NYRKIO_USERNAME")
+    password = os.environ.get("NYRKIO_PASSWORD")
+
+    if not username or not password:
+        print("NYRKIO_USERNAME and NYRKIO_PASSWORD must be set")
+        sys.exit(1)
+
+    # Get JWT token
+    response = requests.post(
+        "https://nyrk.io/api/v0/auth/jwt/login",
+        data={"username": username, "password": password},
+    )
+    response.raise_for_status()
+    token = response.json()["access_token"]
+
+    post_data = {}
+    test_names = []
+    for r in results["benchmarks"]:
+        test_name = r["fullname"]
+        # Convert :: to / to use Nyrkiö's hierarchical navigation and
+        # grouping of results.
+        test_name = test_name.replace("::", "/")
+        test_names.append(test_name)
+
+        payload = create_nyrkio_payload(results["commit_info"], r)
+        post_data[test_name] = [payload]
+
+    for test_name in test_names:
+        submit_results(test_name, post_data[test_name], token)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: process_results.py <filename>")
+        sys.exit(1)
+
+    main(sys.argv[1])
---
+++ backend/benches/test_default_data.py
@@ -0,0 +1,32 @@
+import requests
+import os
+
+# Must include the protocol prefix and port number if not 80
+HOST = os.environ.get("TEST_HOST", "http://localhost")
+
+
+def test_default_data_results(benchmark):
+    """
+    Run a HTTP GET on the default data results endpoint.
+    """
+
+    def fetch():
+        response = requests.get(f"{HOST}/api/v0/default/results")
+        response.raise_for_status()
+
+    benchmark(fetch)
+
+
+def test_default_data_changes(benchmark):
+    """
+    Run a HTTP GET on the default data changes endpoint.
+    """
+    response = requests.get(f"{HOST}/api/v0/default/results")
+    response.raise_for_status()
+    test_name = response.json()[0]
+
+    def fetch(test_name):
+        response = requests.get(f"{HOST}/api/v0/default/result/{test_name}/changes")
+        response.raise_for_status()
+
+    benchmark(fetch, test_name)
