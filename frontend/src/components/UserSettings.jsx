fb16290e9fde452ed69ebc842bd835ebd4726e6e
Matt Fleming <matt@nyrk.io>
2024-02-21 14:15:58 +0000

    frontend/user-settings: Use default magnitude and p-value values
    If the user hasn't specified min magnitude and max p-value values yet
    (because they logged in for the first time) then provide default values.
    
    Fixes #111

--- frontend/src/components/UserSettings.jsx
+++ frontend/src/components/UserSettings.jsx
@@ -2,25 +2,29 @@ import { set } from "date-fns";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-
 export const UserSettings = () => {
   return (
     <>
       <div className="container">
-      <HunterSettings />
-      <SlackSettings />
+        <HunterSettings />
+        <SlackSettings />
       </div>
     </>
   );
-
 };
-
 
 const HunterSettings = () => {
   const saveHunterSettings = async () => {
-    const minMagnitude = getRealMinMagnitude(document.getElementById("nyrkio-min-magnitude-slider").value)/100.0;
-    const pValue = getRealPValue(document.getElementById("nyrkio-p-value-slider").value);
-    const configObject = {core: {min_magnitude: minMagnitude, max_pvalue: pValue}};
+    const minMagnitude =
+      getRealMinMagnitude(
+        document.getElementById("nyrkio-min-magnitude-slider").value
+      ) / 100.0;
+    const pValue = getRealPValue(
+      document.getElementById("nyrkio-p-value-slider").value
+    );
+    const configObject = {
+      core: { min_magnitude: minMagnitude, max_pvalue: pValue },
+    };
     console.debug("POST /api/v0/user/config ");
     console.debug(configObject);
 
@@ -35,12 +39,13 @@ const HunterSettings = () => {
     if (response.status !== 200) {
       console.error("Failed to POST Nyrkiö core user settings");
       console.log(response);
-    }
-    else console.debug(response);
+    } else console.debug(response);
   };
 
   const getHunterSettings = async () => {
-    console.debug("GET /api/v0/user/config")
+    console.debug("GET /api/v0/user/config");
+    // TODO(mfleming) It'd be nice to not hard code this.
+    const defaultConfig = { min_magnitude: 0.05, max_pvalue: 0.001 };
     const response = await fetch("/api/v0/user/config", {
       headers: {
         "Content-type": "application/json",
@@ -50,150 +55,165 @@ const HunterSettings = () => {
     if (response.status !== 200) {
       console.error("Failed to GET Nyrkiö core user settings");
       console.log(response);
-    }
-    else console.debug(response);
+      return defaultConfig;
+    } else console.debug(response);
     const data = await response.json();
     console.debug(data);
-    if (Object.keys(data).length > 0 && Object.keys(data.core).length > 0) {
+    if (
+      Object.keys(data).length > 0 &&
+      data.hasOwnProperty("core") &&
+      Object.keys(data.core).length > 0
+    ) {
       //minMagnitudeSet(data.core.min_magnitude);
       //pvalueSet(data.core.max_pvalue);
       return data.core;
     }
-    else {
-      return {min_magnitude: "Network error", max_pvalue: "Network error"};
-    }
+
+    return defaultConfig;
   };
 
   // Use logarithmic mode to allow for more granularity around 0.5 - 5 %.
   const minMagnitudeUpdate = (rawValue) => {
-      document.getElementById("nyrkio-min-magnitude-value").innerHTML=getRealMinMagnitude(rawValue);
-      saveHunterSettings();
+    document.getElementById("nyrkio-min-magnitude-value").innerHTML =
+      getRealMinMagnitude(rawValue);
+    saveHunterSettings();
   };
   const getRealMinMagnitude = (rawValue) => {
-      const scaledDown=rawValue/1000.0;
-      const logScale=((Math.pow(scaledDown,4)  ))/100;
-      const quantized = parseFloat((Math.round(logScale*2)/2.0).toPrecision(2 ));
-      //console.debug("mrawreal " + rawValue + " " + quantized + " " + getRawMinMagnitude(logScale) + " " + getRawMinMagnitude(quantized));
-      return quantized;
+    const scaledDown = rawValue / 1000.0;
+    const logScale = Math.pow(scaledDown, 4) / 100;
+    const quantized = parseFloat(
+      (Math.round(logScale * 2) / 2.0).toPrecision(2)
+    );
+    //console.debug("mrawreal " + rawValue + " " + quantized + " " + getRawMinMagnitude(logScale) + " " + getRawMinMagnitude(quantized));
+    return quantized;
   };
   const getRawMinMagnitude = (realValue) => {
-      const scaledDown = Math.pow(realValue*100,1.0/4.0);
-      const rawValue = scaledDown*1000.0;
-      return rawValue;
+    const scaledDown = Math.pow(realValue * 100, 1.0 / 4.0);
+    const rawValue = scaledDown * 1000.0;
+    return rawValue;
   };
   const minMagnitudeSet = (realValue) => {
-      const rawValue=getRawMinMagnitude(realValue);
-      document.getElementById("nyrkio-min-magnitude-value").innerHTML=Math.round(realValue);
-      document.getElementById("nyrkio-min-magnitude-slider").value=rawValue;
-      return rawValue;
+    const rawValue = getRawMinMagnitude(realValue);
+    document.getElementById("nyrkio-min-magnitude-value").innerHTML =
+      Math.round(realValue);
+    document.getElementById("nyrkio-min-magnitude-slider").value = rawValue;
+    return rawValue;
   };
 
   const pvalueUpdate = (rawValue) => {
-      document.getElementById("nyrkio-p-value-value").innerHTML=getRealPValue(rawValue);
-      saveHunterSettings();
+    document.getElementById("nyrkio-p-value-value").innerHTML =
+      getRealPValue(rawValue);
+    saveHunterSettings();
   };
   const getRealPValue = (rawValue) => {
-      const scaledDown=rawValue/100.0;
-      const logScale=((Math.pow(scaledDown,3)))/1000;
-      const quantized =parseFloat((Math.round(logScale)).toPrecision(1 ))/1000.0;
-      //console.debug("prawreal " + rawValue + " " + quantized + " " + getRawPValue(logScale) + " " + getRawPValue(quantized));
-      return quantized;
+    const scaledDown = rawValue / 100.0;
+    const logScale = Math.pow(scaledDown, 3) / 1000;
+    const quantized = parseFloat(Math.round(logScale).toPrecision(1)) / 1000.0;
+    //console.debug("prawreal " + rawValue + " " + quantized + " " + getRawPValue(logScale) + " " + getRawPValue(quantized));
+    return quantized;
   };
   const getRawPValue = (realValue) => {
-      const scaledDown = Math.pow(realValue*1000,1.0/3.0);
-      const rawValue = scaledDown*100.0*10;
-      return rawValue;
+    const scaledDown = Math.pow(realValue * 1000, 1.0 / 3.0);
+    const rawValue = scaledDown * 100.0 * 10;
+    return rawValue;
   };
   const pvalueSet = (realValue) => {
-    const rawValue=getRawPValue(realValue);
-      document.getElementById("nyrkio-p-value-value").innerHTML=realValue;
-      document.getElementById("nyrkio-p-value-slider").value=rawValue;
-      return rawValue;
+    const rawValue = getRawPValue(realValue);
+    document.getElementById("nyrkio-p-value-value").innerHTML = realValue;
+    document.getElementById("nyrkio-p-value-slider").value = rawValue;
+    return rawValue;
   };
 
   const NyrkioCpSliders = () => {
-
     return (
       <>
         <div id="nyrkio-cp-sliders">
-            <div className="row">
-                <div className="col col-md-12">
-                <label htmlFor="nyrkio-min-magnitude-slider" className="form-label">Change point threshold: </label>
-                </div>
-                <div className="col col-md-10">
-                <input type="range"
-                  id="nyrkio-min-magnitude-slider"
-                  name="nyrkio-min-magnitude-slider"
-                  className="nyrkio-min-magnitude-slider nyrkio-slider"
-                  style={{width: "100%"}}
-                  defaultValue={0}
-                  min={0}
-                  max={10000}
-                  step={50}
-                  precision={50}
-                  tooltip="off"
-                  onChange={ev => minMagnitudeUpdate(ev.target.value)}
-                />
-              </div>
-              <div className="col col-md-2">
-                <span id="nyrkio-min-magnitude-value" className="form-label">{ 0 }</span><span className="form-label">%</span>
-              </div>
+          <div className="row">
+            <div className="col col-md-12">
+              <label
+                htmlFor="nyrkio-min-magnitude-slider"
+                className="form-label"
+              >
+                Change point threshold:{" "}
+              </label>
             </div>
-            <div className="row mt-5 ">
-              <div className="col col-md-12">
-                <label htmlFor="nyrkio-p-value-slider" className="form-label">P-value threshold: </label>
-                </div>
-                <div className="col col-md-10">
-                <input type="range"
-                  id="nyrkio-p-value-slider"
-                  name="nyrkio-p-value-slider"
-                  className="nyrkio-p-value-slider nyrkio-slider"
-                  style={{width: "100%"}}
-                  defaultValue={0}
-                  min={100}
-                  max={10100}
-                  step={10}
-                  precision={10}
-                  tooltip="off"
-                  onChange={ev => pvalueUpdate(ev.target.value)}
-                />
-              </div>
-              <div className="col col-md-2">
-                <span id="nyrkio-p-value-value" className="form-label">{ 0 }</span>
-              </div>
+            <div className="col col-md-10">
+              <input
+                type="range"
+                id="nyrkio-min-magnitude-slider"
+                name="nyrkio-min-magnitude-slider"
+                className="nyrkio-min-magnitude-slider nyrkio-slider"
+                style={{ width: "100%" }}
+                defaultValue={0}
+                min={0}
+                max={10000}
+                step={50}
+                precision={50}
+                tooltip="off"
+                onChange={(ev) => minMagnitudeUpdate(ev.target.value)}
+              />
             </div>
-        </div>
-      </>
-    );
-
-  };
-
-
-  useEffect(() => {
-    getHunterSettings().then((initialConfig)=>{
-      pvalueSet(initialConfig.max_pvalue);
-      minMagnitudeSet(initialConfig.min_magnitude*100);
-
-    }
-    );
-  }, [NyrkioCpSliders]
-  );
-  return (
-      <div className="row pt-5 justify-content-center">
-        <div className="col-md-8">
-          <div className="card">
-            <div className="card-header">Change Point Detection</div>
-            <div className="card-body">
-                  <p className="card-text">
-                    These settings are global for all your metrics.
-                  </p>
-                  <NyrkioCpSliders />
+            <div className="col col-md-2">
+              <span id="nyrkio-min-magnitude-value" className="form-label">
+                {0}
+              </span>
+              <span className="form-label">%</span>
+            </div>
+          </div>
+          <div className="row mt-5 ">
+            <div className="col col-md-12">
+              <label htmlFor="nyrkio-p-value-slider" className="form-label">
+                P-value threshold:{" "}
+              </label>
+            </div>
+            <div className="col col-md-10">
+              <input
+                type="range"
+                id="nyrkio-p-value-slider"
+                name="nyrkio-p-value-slider"
+                className="nyrkio-p-value-slider nyrkio-slider"
+                style={{ width: "100%" }}
+                defaultValue={0}
+                min={100}
+                max={10100}
+                step={10}
+                precision={10}
+                tooltip="off"
+                onChange={(ev) => pvalueUpdate(ev.target.value)}
+              />
+            </div>
+            <div className="col col-md-2">
+              <span id="nyrkio-p-value-value" className="form-label">
+                {0}
+              </span>
             </div>
           </div>
         </div>
-      </div>
-  );
+      </>
+    );
+  };
 
+  useEffect(() => {
+    getHunterSettings().then((initialConfig) => {
+      pvalueSet(initialConfig.max_pvalue);
+      minMagnitudeSet(initialConfig.min_magnitude * 100);
+    });
+  }, [NyrkioCpSliders]);
+  return (
+    <div className="row pt-5 justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">Change Point Detection</div>
+          <div className="card-body">
+            <p className="card-text">
+              These settings are global for all your metrics.
+            </p>
+            <NyrkioCpSliders />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const SlackSettings = () => {
@@ -249,77 +269,77 @@ const SlackSettings = () => {
   const slackBtnText =
     Object.keys(slackData).length > 0 ? "Re-connect to Slack" : "Add to Slack";
   return (
-      <div className="row pt-5 justify-content-center">
-        <div className="col-md-8">
-          <div className="card">
-            <div className="card-header">Slack</div>
-            <div className="card-body">
-              {Object.keys(slackData).length > 0 ? (
-                <>
-                  <form>
-                    <div className="mb-3 pt-3 col-md-7">
-                      <label htmlFor="slackChannel" className="form-label">
-                        Sending notifications to:
-                      </label>
-                      <div className="input-group">
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="slackName"
-                          value={slackData.team}
-                          disabled
-                        />
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="slackChannel"
-                          value={slackData.channel}
-                          disabled
-                        />
-                      </div>
+    <div className="row pt-5 justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">Slack</div>
+          <div className="card-body">
+            {Object.keys(slackData).length > 0 ? (
+              <>
+                <form>
+                  <div className="mb-3 pt-3 col-md-7">
+                    <label htmlFor="slackChannel" className="form-label">
+                      Sending notifications to:
+                    </label>
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="slackName"
+                        value={slackData.team}
+                        disabled
+                      />
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="slackChannel"
+                        value={slackData.channel}
+                        disabled
+                      />
                     </div>
-                  </form>
-                </>
-              ) : (
-                <>
-                  <p className="card-text">
-                    Nyrkiö can send notifications to Slack when a performance
-                    change is detected.
-                  </p>
-                </>
-              )}
-              <a
-                href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&amp;user_scope=&amp;redirect_uri=https%3A%2F%2Fnyrk.io%2Fuser%2Fsettings&amp;client_id=6044529706771.6587337889574"
-                className="slack-btn"
+                  </div>
+                </form>
+              </>
+            ) : (
+              <>
+                <p className="card-text">
+                  Nyrkiö can send notifications to Slack when a performance
+                  change is detected.
+                </p>
+              </>
+            )}
+            <a
+              href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&amp;user_scope=&amp;redirect_uri=https%3A%2F%2Fnyrk.io%2Fuser%2Fsettings&amp;client_id=6044529706771.6587337889574"
+              className="slack-btn"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 122.8 122.8"
+                height={20}
+                width={20}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 122.8 122.8"
-                  height={20}
-                  width={20}
-                >
-                  <path
-                    d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
-                    fill="#e01e5a"
-                  ></path>
-                  <path
-                    d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
-                    fill="#36c5f0"
-                  ></path>
-                  <path
-                    d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
-                    fill="#2eb67d"
-                  ></path>
-                  <path
-                    d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
-                    fill="#ecb22e"
-                  ></path>
-                </svg>
-                {slackBtnText}
-              </a>
-            </div>
+                <path
+                  d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
+                  fill="#e01e5a"
+                ></path>
+                <path
+                  d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
+                  fill="#36c5f0"
+                ></path>
+                <path
+                  d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
+                  fill="#2eb67d"
+                ></path>
+                <path
+                  d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
+                  fill="#ecb22e"
+                ></path>
+              </svg>
+              {slackBtnText}
+            </a>
           </div>
         </div>
       </div>
+    </div>
   );
 };
