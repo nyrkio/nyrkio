e10a16989ba0d716175ad6a1c6d8a3906ed16cec
Matt Fleming <matt@nyrk.io>
2024-03-06 21:39:21 +0000

    Remove non-open source logos from repo

--- frontend/src/components/LegendPage.jsx
+++ frontend/src/components/LegendPage.jsx
@@ -1,10 +1,6 @@
 import tigerbeetle from "../static/tigerbeetle-change-points.png";
 import grafanaDashboard from "../static/Grafana_dashboard.png";
 import image1 from "../static/50776727286_cdd63903f4_w.jpg";
-import hornImage from "../static/nyyrikki/nyyrikki-horn2.png";
-import nyyrikkiImage from "../static/nyyrikki/nyyrikki-low-res.png";
-
-
 
 export const LegendPage = () => {
   document.body.classList.add("nyyrikki-banner-right-bw");
@@ -13,112 +9,274 @@ export const LegendPage = () => {
     <>
       <div className="container nyyrikki-legend">
         <div className="row mt-8 p-5">
-            <h1 class="font-weight-bolder text-center nyrkio-accent-dark">
-              Nyyrikki
-            </h1>
-            <h2 class="text-center nyrkio-accent-dark">
-              The legend
-            </h2>
-          </div>
-          <div class="row col-lg-12 mt-8">
-            <h3>A spirit has many names</h3>
-            <p>
-              Nyrkiö is one of the many names of <span class="nyrkio-accent-dark">Nyyrikki</span>, the ancient spirit of hunting of the Finnish and Karelian conifer forests. They are the son of Tapio, the king of the forest, and Mielikki the protective and kind mother of the forest kingdom. They also have a sister, Tellervo - a playful and beautiful young girl, who nevertheless is assigned responsibility for cattle and the birds.
-            </p>
-            <p>The forest is a holy place for the Fennic tribes (Hämäläiset, Savolaiset, Karjalaiset, Komi and Mari, and of course the Sami). It provides food and it provides shelter from warring tribes, or ships passing on the sea. It is also where their ancestors are buried. The trees and animals all have a spirit of their own, and humans hug the trees and even bid them offerings when entering the forest. Or <span class="nyrkio-accent-dark">Tapiola</span> - the reigns of Tapio.
-            </p>
-            <p>
-              Nyrkiö is known under many names beyond just <span class="nyrkio-accent-dark">Nyyrikki</span>. A Latin manuscript uses the variant <em>Nyrckes</em>, which hence can be thought of as the international variation of the name. Although in most songs Nyyrikki is introduced as the <em>son</em> of Tapio, they are sometimes also witnessed to have taken a more feminine corpus when interacting with humans. In fact the "kki" ending in the name suggests feminine association, while Nyrckes, Nyrkiö and others are masculin names. Hence we use <a href="https://en.wikipedia.org/wiki/Singular_they">singular they</a> as the pronoun when referring to <span class="nyrkio-accent-dark">Nyyrikki</span>. This is of course purely as guidance for our international readers, as <span class="nyrkio-accent-dark">Nyyrikki</span> themselves remains blissfully unaware of gendered pronouns... <a href="https://finland.fi/han/article/">Finnish and Finno-Ugric languages don't have them</a>.
-            </p>
+          <h1 class="font-weight-bolder text-center nyrkio-accent-dark">
+            Nyyrikki
+          </h1>
+          <h2 class="text-center nyrkio-accent-dark">The legend</h2>
+        </div>
+        <div class="row col-lg-12 mt-8">
+          <h3>A spirit has many names</h3>
+          <p>
+            Nyrkiö is one of the many names of{" "}
+            <span class="nyrkio-accent-dark">Nyyrikki</span>, the ancient spirit
+            of hunting of the Finnish and Karelian conifer forests. They are the
+            son of Tapio, the king of the forest, and Mielikki the protective
+            and kind mother of the forest kingdom. They also have a sister,
+            Tellervo - a playful and beautiful young girl, who nevertheless is
+            assigned responsibility for cattle and the birds.
+          </p>
+          <p>
+            The forest is a holy place for the Fennic tribes (Hämäläiset,
+            Savolaiset, Karjalaiset, Komi and Mari, and of course the Sami). It
+            provides food and it provides shelter from warring tribes, or ships
+            passing on the sea. It is also where their ancestors are buried. The
+            trees and animals all have a spirit of their own, and humans hug the
+            trees and even bid them offerings when entering the forest. Or{" "}
+            <span class="nyrkio-accent-dark">Tapiola</span> - the reigns of
+            Tapio.
+          </p>
+          <p>
+            Nyrkiö is known under many names beyond just{" "}
+            <span class="nyrkio-accent-dark">Nyyrikki</span>. A Latin manuscript
+            uses the variant <em>Nyrckes</em>, which hence can be thought of as
+            the international variation of the name. Although in most songs
+            Nyyrikki is introduced as the <em>son</em> of Tapio, they are
+            sometimes also witnessed to have taken a more feminine corpus when
+            interacting with humans. In fact the "kki" ending in the name
+            suggests feminine association, while Nyrckes, Nyrkiö and others are
+            masculin names. Hence we use{" "}
+            <a href="https://en.wikipedia.org/wiki/Singular_they">
+              singular they
+            </a>{" "}
+            as the pronoun when referring to{" "}
+            <span class="nyrkio-accent-dark">Nyyrikki</span>. This is of course
+            purely as guidance for our international readers, as{" "}
+            <span class="nyrkio-accent-dark">Nyyrikki</span> themselves remains
+            blissfully unaware of gendered pronouns...{" "}
+            <a href="https://finland.fi/han/article/">
+              Finnish and Finno-Ugric languages don't have them
+            </a>
+            .
+          </p>
 
           <div class="row mt-4 my-sm-4 text-center ">
-              <center><img src={nyyrikkiImage} className="rounded-3 shadow-blur nyyrikki-original-art" /></center>
-              <p><small>All Nyyrikki imagery on this site by <a href="https://www.deviantart.com/tuherrus/gallery">tuherrus of Deviant Art</a>.</small></p>
+            <center>
+              <img
+                src="https://nyrkio.com/p/nyyrikki/nyyrikki-low-res.png"
+                className="rounded-3 shadow-blur nyyrikki-original-art"
+              />
+            </center>
+            <p>
+              <small>
+                All Nyyrikki imagery on this site by{" "}
+                <a href="https://www.deviantart.com/tuherrus/gallery">
+                  tuherrus of Deviant Art
+                </a>
+                .
+              </small>
+            </p>
           </div>
 
           <div class="row col-lg-12 mt-4">
-          <h3>The spirit of hunters</h3>
-          <p>In the ancient Fenno-Ugric world, each place and thing has its own god or spirit: Ukko ylijumala in the sky, Ahti in the water, and Tapio with his family members as caretakers of the forest. The word <em>haltia</em> is the same as Swedish älva, or Irish/Celtic Elfs. However, the word means - or at least rhymes with - the word guardian. Hence Tapio is the guardian and king of the entire forest, the kingdom of Tapiola.</p>
-          <p>And in that kingdom, <span class="nyrkio-accent-dark">Nyyrikki</span> is assigned the caretaker role for small game, in particular the squirrels.</p>
-          <p>While seemingly a lesser task in the family*,  <span class="nyrkio-accent-dark">Nyyrikki</span> is known to be a loyal and committed guardian spirit, caring well for their creatures. And so it was the case, that eventually the small squirrels, and particularly their skin, was considered the most precious possession a hunter could catch in the forest. (Second only to literal gold and silver, the domain of Nyyrikki's mother, Mielikki.) So much so that squirrel skins still today is a slang word for money, as they were used as an early form of currency once upon a time.</p>
+            <h3>The spirit of hunters</h3>
+            <p>
+              In the ancient Fenno-Ugric world, each place and thing has its own
+              god or spirit: Ukko ylijumala in the sky, Ahti in the water, and
+              Tapio with his family members as caretakers of the forest. The
+              word <em>haltia</em> is the same as Swedish älva, or Irish/Celtic
+              Elfs. However, the word means - or at least rhymes with - the word
+              guardian. Hence Tapio is the guardian and king of the entire
+              forest, the kingdom of Tapiola.
+            </p>
+            <p>
+              And in that kingdom,{" "}
+              <span class="nyrkio-accent-dark">Nyyrikki</span> is assigned the
+              caretaker role for small game, in particular the squirrels.
+            </p>
+            <p>
+              While seemingly a lesser task in the family*,{" "}
+              <span class="nyrkio-accent-dark">Nyyrikki</span> is known to be a
+              loyal and committed guardian spirit, caring well for their
+              creatures. And so it was the case, that eventually the small
+              squirrels, and particularly their skin, was considered the most
+              precious possession a hunter could catch in the forest. (Second
+              only to literal gold and silver, the domain of Nyyrikki's mother,
+              Mielikki.) So much so that squirrel skins still today is a slang
+              word for money, as they were used as an early form of currency
+              once upon a time.
+            </p>
 
-          <p>When entering the forest, a hunter should sing a suitable song** or poem, essentially a long form spell, to appease the hunter spirit <span class="nyrkio-accent-dark">Nyyrikki</span>.</p>
+            <p>
+              When entering the forest, a hunter should sing a suitable song**
+              or poem, essentially a long form spell, to appease the hunter
+              spirit <span class="nyrkio-accent-dark">Nyyrikki</span>.
+            </p>
 
-          <blockquote>
-"Nyyrikki, Tapion poika,<br/>
-mies puhas, punakypärä!<br/>
-Veistä pilkut pitkin maita,<br/>
-rastit vaaroihin rakenna,<br/>
-jotta tunnen, tuhma, käyä,<br/>
-äkkiouto, tien osoan<br/>
-etsiessäni ereä,<br/>
-antia anellessani! <br/>
-<br/>
-<em>translation</em>
-<br/>
-Nyrckes, of Tapio,<br/>
-In your red and shining helmet<br/>
-Make your arrows show a path<br/>
-Find your shelters in the mountain<br/>
-So that I can find my ways<br/>
-Ignorant as I might be<br/>
-Seeking for something wild<br/>
-Praying for a generous catch<br/>
-          </blockquote>
+            <blockquote>
+              "Nyyrikki, Tapion poika,
+              <br />
+              mies puhas, punakypärä!
+              <br />
+              Veistä pilkut pitkin maita,
+              <br />
+              rastit vaaroihin rakenna,
+              <br />
+              jotta tunnen, tuhma, käyä,
+              <br />
+              äkkiouto, tien osoan
+              <br />
+              etsiessäni ereä,
+              <br />
+              antia anellessani! <br />
+              <br />
+              <em>translation</em>
+              <br />
+              Nyrckes, of Tapio,
+              <br />
+              In your red and shining helmet
+              <br />
+              Make your arrows show a path
+              <br />
+              Find your shelters in the mountain
+              <br />
+              So that I can find my ways
+              <br />
+              Ignorant as I might be
+              <br />
+              Seeking for something wild
+              <br />
+              Praying for a generous catch
+              <br />
+            </blockquote>
           </div>
 
           <div class="row col-lg-12 mt-4">
-          <p>When properly summonned, a hunter could then expect <span class="nyrkio-accent-dark">Nyyrikki</span> to reveal a squirrel or some other pray up on a branch. And <span class="nyrkio-accent-dark">Nyyrikki</span> will also put some <span class="nyrkio-accent-dark">markers</span> in front of the hunter, so that they will see a clear path out of the dark forests.</p>
+            <p>
+              When properly summonned, a hunter could then expect{" "}
+              <span class="nyrkio-accent-dark">Nyyrikki</span> to reveal a
+              squirrel or some other pray up on a branch. And{" "}
+              <span class="nyrkio-accent-dark">Nyyrikki</span> will also put
+              some <span class="nyrkio-accent-dark">markers</span> in front of
+              the hunter, so that they will see a clear path out of the dark
+              forests.
+            </p>
           </div>
 
           <div class="row col-lg-12 mt-4" className="nyyrikki-last-text">
-          <img src={hornImage} className="nyyrikki-horn-right"/>
-          <p><em>And that, my friends... Is exactly what we do at <a href="/product">Nyrkiö</a>.</em></p>
+            <img
+              src="https://nyrkio.com/p/nyyrikki/nyyrikki-horn2.png"
+              className="nyyrikki-horn-right"
+            />
+            <p>
+              <em>
+                And that, my friends... Is exactly what we do at{" "}
+                <a href="/product">Nyrkiö</a>.
+              </em>
+            </p>
           </div>
 
           <div class="row col-lg-12 mt-12">
-          <p><small>*) ...much like being on the Quality &amp; Performance team, you might think?</small></p>
-          <p><small>**) The Finnish and Baltic folk songs are always in <a href="https://en.wikipedia.org/wiki/Trochaic_tetrameter#Kalevala_meter">Kalevala meter</a>. That is, 5 words (or "throchaic feet") per line, instead of the four used across<br />
-          western civilization back to ancient Greece. And then using rhymes in the first syllables rather than last. It's easy, just think of it as a haiku where all lines must be exactly 5 words, each with 2 syllables!</small></p>
+            <p>
+              <small>
+                *) ...much like being on the Quality &amp; Performance team, you
+                might think?
+              </small>
+            </p>
+            <p>
+              <small>
+                **) The Finnish and Baltic folk songs are always in{" "}
+                <a href="https://en.wikipedia.org/wiki/Trochaic_tetrameter#Kalevala_meter">
+                  Kalevala meter
+                </a>
+                . That is, 5 words (or "throchaic feet") per line, instead of
+                the four used across
+                <br />
+                western civilization back to ancient Greece. And then using
+                rhymes in the first syllables rather than last. It's easy, just
+                think of it as a haiku where all lines must be exactly 5 words,
+                each with 2 syllables!
+              </small>
+            </p>
           </div>
 
           <div class="row col-lg-12 mt-8">
             <h2>Bibliography</h2>
             <ol>
-            <li>Uno Holmberg, 1927. <a href="https://wordandsilence.com/wp-content/uploads/2017/11/mythology-of-all-races-4-finno-ugric-siberian.pdf">Mythology of all races, vol 4</a>. <br/>(Warmly recommend this book, I learned a lot that they don't teach or speak about nowadays in mainstream sources.)</li>
+              <li>
+                Uno Holmberg, 1927.{" "}
+                <a href="https://wordandsilence.com/wp-content/uploads/2017/11/mythology-of-all-races-4-finno-ugric-siberian.pdf">
+                  Mythology of all races, vol 4
+                </a>
+                . <br />
+                (Warmly recommend this book, I learned a lot that they don't
+                teach or speak about nowadays in mainstream sources.)
+              </li>
 
-            <li><a href="http://www.nauhalainen.fi/wp/suomalaisia-jumalia-ja-haltioita-osa-4-nyyrikki/">Finnish Gods and Spirits, Nyyrikki</a></li>
+              <li>
+                <a href="http://www.nauhalainen.fi/wp/suomalaisia-jumalia-ja-haltioita-osa-4-nyyrikki/">
+                  Finnish Gods and Spirits, Nyyrikki
+                </a>
+              </li>
 
-            <li><a href="https://web.archive.org/web/20150725020812/http://kansanrunous.net84.net/kalevala/14.htm">Poem 14.</a> (covers Nyyrikki and other forest and hunting related voaublary.</li>
+              <li>
+                <a href="https://web.archive.org/web/20150725020812/http://kansanrunous.net84.net/kalevala/14.htm">
+                  Poem 14.
+                </a>{" "}
+                (covers Nyyrikki and other forest and hunting related voaublary.
+              </li>
             </ol>
-
 
             <h2>Youtube</h2>
 
-            <p>Just some samples of Kalevala songs. Can you count 5 on each line?</p>
-            <p>The instrument used is a <span class="nyrkio-accent-dark">kantele</span>, a five string harp. In the epic it is used by the powerful sorcerer <strong>Väinämöinen</strong> himself to cast - sing - his spells.</p>
-
+            <p>
+              Just some samples of Kalevala songs. Can you count 5 on each line?
+            </p>
+            <p>
+              The instrument used is a{" "}
+              <span class="nyrkio-accent-dark">kantele</span>, a five string
+              harp. In the epic it is used by the powerful sorcerer{" "}
+              <strong>Väinämöinen</strong> himself to cast - sing - his spells.
+            </p>
 
             <ol>
+              <li>
+                <a href="https://www.youtube.com/watch?v=KXrHvDRatSA">
+                  Kalevala, poem 10.
+                </a>
+              </li>
 
-            <li><a href="https://www.youtube.com/watch?v=KXrHvDRatSA">Kalevala, poem 10.</a></li>
+              <li>
+                <a href="https://www.youtube.com/watch?v=hFCixLn9qRw">
+                  Nuku nuku nurmilintu
+                </a>
+                , Merja Soria. (Lullaby)
+              </li>
 
+              <li>
+                <a href="https://www.youtube.com/watch?v=A6z7q4mwJq4">
+                  Forest folklore in Finland.
+                </a>
+                ("Metsänpeitto" - covered by the forest.)
+              </li>
 
-            <li><a href="https://www.youtube.com/watch?v=hFCixLn9qRw">Nuku nuku nurmilintu</a>, Merja Soria. (Lullaby)</li>
+              <li>
+                <a href="https://www.youtube.com/watch?v=_xDuex12uz0">
+                  Impact on Finnish mythology on J.R.R Tolkien
+                </a>
+              </li>
 
-            <li><a href="https://www.youtube.com/watch?v=A6z7q4mwJq4">Forest folklore in Finland.</a>("Metsänpeitto" - covered by the forest.)</li>
-
-            <li><a href="https://www.youtube.com/watch?v=_xDuex12uz0">Impact on Finnish mythology on J.R.R Tolkien</a></li>
-
-            <li><a href="https://www.youtube.com/watch?v=t74bPt4xRNE">Kalevalamitta</a><br/>
-                A song about Kalevala meter, itself 100% in Kalevala-meter. Hopefully we can translate this to English one day.</li>
-
+              <li>
+                <a href="https://www.youtube.com/watch?v=t74bPt4xRNE">
+                  Kalevalamitta
+                </a>
+                <br />A song about Kalevala meter, itself 100% in
+                Kalevala-meter. Hopefully we can translate this to English one
+                day.
+              </li>
             </ol>
-
           </div>
         </div>
       </div>
-
     </>
   );
 };
--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,18 +1,23 @@
-import defaultLogo from '../static/logo/full/Brown/NyrkioLogo_Final_Full_Brown-800px.png';
-import smallLogo from '../static/logo/full/Brown/NyrkioLogo_Final_Full_Brown-200px.png';
+export const Logo = () => {
+  return (
+    <div className="nyrkio-logo nyrkio-logo-default">
+      <img
+        src="https://nyrkio.com/p/logo/full/Brown/NyrkioLogo_Final_Full_Brown-800px.png"
+        alt="Nyrkiö (logo)"
+        className="nyrkio-logo-img nyrkio-logo-img-default"
+      />
+    </div>
+  );
+};
 
-export const Logo =  ( ) => {
-        return (
-            <div className="nyrkio-logo nyrkio-logo-default">
-            <img src={defaultLogo} alt="Nyrkiö (logo)" className="nyrkio-logo-img nyrkio-logo-img-default" />
-            </div>
-        );
-}
-
-export const SmallLogo =  ( ) => {
-        return (
-            <div className="nyrkio-logo nyrkio-logo-default">
-            <img src={smallLogo} alt="Nyrkiö (logo)" className="nyrkio-logo-img nyrkio-logo-img-small " />
-            </div>
-        );
-}
+export const SmallLogo = () => {
+  return (
+    <div className="nyrkio-logo nyrkio-logo-default">
+      <img
+        src="https://nyrkio.com/p/logo/full/Brown/NyrkioLogo_Final_Full_Brown-200px.png"
+        alt="Nyrkiö (logo)"
+        className="nyrkio-logo-img nyrkio-logo-img-small "
+      />
+    </div>
+  );
+};
