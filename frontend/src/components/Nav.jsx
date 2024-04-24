c11831d8da38d4bafd777c8765425a3dbdb83be1
Henrik Ingo <henrik@nyrk.io>
2024-04-24 20:01:40 +0300

    Fix /about link in Navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -39,7 +39,7 @@ const NavigationItems = () => {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/aboutus" title="About Nyrkiö">
+            <a className="nav-link" href="/about" title="About Nyrkiö">
               About
             </a>
           </li>
