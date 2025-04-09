212668c77bee760ba40b0890bca1c61b77c37ceb
Henrik Ingo <henrik@nyrk.io>
2025-04-10 01:18:11 +0300

    Add webhook for github marketplace

---
+++ backend/github/marketplace.py
@@ -0,0 +1,12 @@
+import logging
+from backend.db.db import DBStore
+from FastAPI import APIRouter
+from typing import Dict
+
+market_router = APIRouter(prefix="/github")
+
+@market_router.post("/marketplace-events")
+async def marketplace_events(gh_event: Dict):
+    store = DBStore()
+    store.log_json_event(gh_event, "GitHub Marketplace Webhook")
+    return {"success": "Thank you for using Nyrkiö. For Faster Software!"}
