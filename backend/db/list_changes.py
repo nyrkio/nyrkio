7e615c974e08cc9854f3c1cc08714c5743d313cf
Henrik Ingo <henrik@nyrk.io>
2025-04-30 11:17:18 +0300

    DBStore is not the mongodb db...

--- backend/db/list_changes.py
+++ backend/db/list_changes.py
@@ -7,8 +7,8 @@ from backend.db.db import DBStore
 async def change_points_per_commit(
     user_or_org_id: Any, test_name_prefix: str, commit: str = None
 ):
-    db = DBStore()
-    coll = db.v_valid_change_points
+    store = DBStore()
+    coll = store.db.v_valid_change_points
 
     # the match is not on arbitrary prefix, rather only on full "parts", that is,
     # if this was a path to something then each part is a directory name.
