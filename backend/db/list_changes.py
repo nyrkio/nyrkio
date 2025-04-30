ef74fad248b0d121a9482f869c8f02f568b2b7a8
Henrik Ingo <henrik@nyrk.io>
2025-04-30 11:48:43 +0300

    None vs []

--- backend/db/list_changes.py
+++ backend/db/list_changes.py
@@ -17,8 +17,8 @@ async def change_points_per_commit(
     query = _set_parameters(user_or_org_id, test_name_prefix, commit)
 
     res = await store.get_collection_valid_change_points(query)
-    docs = res.to_list([])
-    return docs
+    docs = res.to_list(None)
+    return docs if docs is not None else []
 
 
 CHANGE_POINTS_PER_COMMIT = [
