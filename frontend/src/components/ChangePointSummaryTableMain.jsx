7c32441007e256e7cd49938ae33e9405d6b67501
Henrik Ingo <henrik@nyrk.io>
2025-05-22 04:58:53 +0300

    Don't repeat same date & commit in the change point table
    Empty space = steve jobs would have been proud

--- frontend/src/components/ChangePointSummaryTableMain.jsx
+++ frontend/src/components/ChangePointSummaryTableMain.jsx
@@ -77,14 +77,15 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
       if (directionMap[metric]=="lower_is_better") return <span title="lower is better">⇩</span>;
   }
 
-  console.debug(changeData);
+  let previousRow = null;
+  //   console.debug(changeData);
   Object.entries(changeData).forEach(([shortName, obj]) => {
     obj.forEach((changePoint) => {
-        console.debug(changePoint);
+        // console.debug(changePoint);
         const branchName = changePoint["attributes"]["branch"]
         const test_name = changePoint["test_name"];
         const changes = changePoint["cp_values"];
-        console.debug(changes);
+        // console.debug(changes);
         changes.map((change) => {
           const commit = changePoint["_id"]["git_commit"];
 
@@ -96,13 +97,27 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
           const repo = changePoint["attributes"]["git_repo"];
           const changeValue = Math.round(change["forward_change_percent"]*100)/100;
           const metric_name = change["metric"];
+          const date = parseTimestamp(changePoint["time"]);
+
+          const isSame = {date:false, commit: false, test: false, index: 0 };
+          if (previousRow !== null){
+            isSame.date = (previousRow.time == changePoint.time);
+            isSame.commit = (previousRow["_id"]["git_commit"] == changePoint["_id"]["git_commit"]);
+            isSame.test = (previousRow["test_name"] == changePoint["test_name"]);
+          }
+          if (isSame.date && isSame.commit){
+            changePoint["__isSameIndex"] = 1 + (previousRow["__isSameIndex"] === undefined ? 0 : previousRow["__isSameIndex"]);
+            isSame.index += changePoint["__isSameIndex"];
+          }
+
           rowData.push({
-            date: parseTimestamp(changePoint["time"]),
-            commit: { commit, commit_msg, repo },
-            test: { test_name, branchName },
+            date: { date, isSame },
+            commit: { commit, commit_msg, repo, isSame },
+            test: { test_name, branchName, isSame },
             metric: { test_name, metric_name, branchName },
             change: { changeValue, metric_name }
           });
+          previousRow = changePoint;
       });
     });
   });
@@ -121,28 +136,46 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
   const colDefs = [
     { field: "date", sort: "desc",
       cellRenderer: (params) => {
-        const text = params.value;
+        const text = params.value.date;
+        const isSame = params.value.isSame;
         if (text == queryStringTextTimestamp){
           return (<b>{text}</b>);
         }
-        return text;
-      }
+        if (params.column.sort != "desc"){
+          isSame.userSort = true;
+        }
+        if (isSame.date && params.column.sort == "desc") {
+          return "";
+        }
+        else {
+          return text;
+        }
+      },
+      valueFormatter: (params)=>{
+        return params.value.date;
+      },
     },
     { field: "test",
       cellRenderer: (params) => {
         let test_name = params.value.test_name;
         const branchName = params.value.branchName;
+        const isSame = params.value.isSame;
         let url = baseUrls.resultsWithOrg + "/" + test_name;
-        console.log(baseUrls);
+        // console.log(baseUrls);
         if (baseUrls.results=="/public"){
           url = baseUrls.resultsWithOrg + "/" + branchName + "/" + test_name;
         }
 
-        return (
-          <>
-          <a href={url}>{test_name}</a>
-          </>
-        );
+        if (isSame.date && isSame.test && ! isSame.userSort) {
+          return "";
+        }
+        else {
+          return (
+            <>
+            <a href={url}>{test_name}</a>
+            </>
+          );
+        }
       },
       valueFormatter: (params)=>{
         return params.value.test_name;
@@ -182,14 +215,20 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
       field: "commit",
       cellRenderer: (params) => {
         const { commit, commit_msg, repo } = params.value;
+        const isSame = params.value.isSame;
 
         const url = repo + "/commit/" + commit;
         const text = formatCommit(commit, commit_msg);
-        return (
-          <a href={url} target="_blank">
-            {text}
-          </a>
-        );
+        if (isSame.date && isSame.test && ! isSame.userSort) {
+          return "";
+        }
+        else {
+          return (
+            <a href={url} target="_blank">
+              {text}
+            </a>
+          );
+        }
       },
       valueFormatter: (params)=>{
         return params.value.commit;
@@ -203,6 +242,33 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
     };
   });
 
+  const rowClassRules = {
+      'ag-row-is-same': (params) => {
+        // console.log(params);
+        const isSame = params.data.date.isSame;
+        return isSame.date && isSame.commit;
+      },
+      'ag-row-is-new': (params) => {
+        // console.log(params);
+        const isSame = params.data.date.isSame;
+        return ! (isSame.date && isSame.commit);
+      }
+  };
+  const getRowStyle = (params) => {
+        const isSame = params.data.date.isSame;
+//         console.log(params.node);
+        if ( (isSame.date && isSame.commit) ){
+          const oldh = params.node.rowHeight;
+          const h = 25;
+          // console.log(isSame);
+          const translate = params.node.rowTop -(oldh -h)*(isSame.index+0.5);
+          params.node.rowHeight = h;
+          return {transform: `translateY(${translate}px)`}
+        }
+//         else {
+//           params.node.rowHeight = 100;
+//         }
+  }
   const gridHeightPx = 135 + Math.min(rowData.length, 50) * 50;
   return (
     <>
@@ -210,12 +276,13 @@ export const ChangePointSummaryTableMain = ({ title, changeData, baseUrls, query
         <h3>{title?title:"Performance Changes"}</h3>
       </div>
       <div
-        className="ag-theme-quartz ag-theme-nyrkio pb-5 col-sm-12 col-lg-12 col-xl-12"
+        className="ag-main-cp-table ag-theme-quartz ag-theme-nyrkio pb-5 col-sm-12 col-lg-12 col-xl-12"
         style={{ width: "100%", height: gridHeightPx, minHeight: "13em", maxHeight: "90vh" }}
       >
         <AgGridReact
           rowData={rowData}
           columnDefs={colDefs}
+          rowClassRules={rowClassRules}
           pagination={true}
           autoSizeStrategy={autoSizeStrategy}
           style={{width: "100%"}}
