14285cf1ecbfd9b83422b6450ebdff4d416a2ff9
Henrik Ingo <henrik@nyrk.io>
2025-07-17 17:05:49 +0300

    Try new slogan
    - Brown full text logo in the menus
    - Trying out new, more descriptive and specific slogan
    - Front page logo fades into the back!

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ export const Logo = () => {
   return (
     <div className="nyrkio-logo nyrkio-logo-default">
     <img
-    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-500.png"
+    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
     alt="Nyrkiö (logo)"
     className="nyrkio-logo-img nyrkio-logo-img-default"
     />
@@ -13,7 +13,7 @@ export const LogoBrown = () => {
   return (
     <div className="nyrkio-logo nyrkio-logo-brown">
     <img
-    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-500.png"
+    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
     alt="Nyrkiö (logo)"
     className="nyrkio-logo-img nyrkio-logo-img-brown"
     />
@@ -25,8 +25,8 @@ export const SmallLogo = () => {
   return (
     <div className="nyrkio-logo nyrkio-logo-default" >
       <img
-        src="/p/logo/full/new/NyrkiooLogo_Final_Logomark_RedTan.png"
-        alt="Nyrkiö (logo)"
+      src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
+      alt="Nyrkiö (logo)"
         title="Nyrkiö"
         className="nyrkio-logo-img nyrkio-logo-img-small "
       />
@@ -38,11 +38,11 @@ export const LogoSlogan = () => {
   return (
     <div className="container-fluid text-center nyrkio-title nyrkio-logo nyrkio-logo-default">
     <img
-    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-500.png"
+    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
     alt="Nyrkiö (logo)"
     className="nyrkio-logo-img nyrkio-logo-img-small "
     />
-    <h1>For Faster Software</h1>
+    <h1><span className="git">git blame</span><span className="for"> for performance</span></h1>
     </div>
   );
 };
@@ -50,11 +50,11 @@ export const LogoBrownSlogan = () => {
   return (
     <div className="container-fluid text-center nyrkio-title nyrkio-logo nyrkio-logo-default">
     <img
-    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-500.png"
+    src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
     alt="Nyrkiö (logo)"
     className="nyrkio-logo-img nyrkio-logo-img-small "
     />
-    <h1>For Faster Software</h1>
+    <h1>git blame for performance</h1>
     </div>
   );
 };
@@ -63,11 +63,11 @@ export const LogoSloganNarrow = () => {
   return (
     <div className="container-fluid text-center nyrkio-title nyrkio-logo nyrkio-logo-default col-sm-12 col-md-10">
       <img
-        src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-500.png"
+        src="/p/logo/full/new/NyrkioLogo_Final_Full_Brown46-shadow-300.png"
         alt="Nyrkiö (logo)"
         className="nyrkio-logo-img nyrkio-logo-img-small "
       />
-      <h1>For Faster Software</h1>
+      <h1>git blame for performance</h1>
     </div>
   );
 };
