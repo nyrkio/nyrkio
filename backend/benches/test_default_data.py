2ed78bad52a3e542f66cb6d7d7c92de830ddb4d3
Henrik Ingo <henrik@nyrk.io>
2025-10-10 13:31:35 +0300

    trigger test 2

--- backend/benches/test_default_data.py
+++ backend/benches/test_default_data.py
@@ -12,6 +12,7 @@ def test_default_data_results(benchmark):
 
     def fetch():
         response = requests.get(f"{HOST}/api/v0/default/results")
+        response = requests.get(f"{HOST}/api/v0/default/results")
         response.raise_for_status()
 
     benchmark(fetch)
@@ -30,4 +31,5 @@ def test_default_data_changes(benchmark):
         response.raise_for_status()
 
     benchmark(fetch, test_name)
+
 
