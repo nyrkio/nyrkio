d0842af561114f560c7cd5c2f80dc4e02a257533
Henrik Ingo <henrik@nyrk.io>
2025-10-10 15:30:17 +0300

    reset perf test

--- backend/benches/test_default_data.py
+++ backend/benches/test_default_data.py
@@ -12,7 +12,6 @@ def test_default_data_results(benchmark):
 
     def fetch():
         response = requests.get(f"{HOST}/api/v0/default/results")
-        response = requests.get(f"{HOST}/api/v0/default/results")
         response.raise_for_status()
 
     benchmark(fetch)
