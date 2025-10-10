cdaafbb6071b55a8ba8c5e5c1bf07753e0ff7f5f
Henrik Ingo <henrik@nyrk.io>
2025-10-10 03:46:33 +0300

    3 s

--- backend/benches/test_default_data.py
+++ backend/benches/test_default_data.py
@@ -14,7 +14,7 @@ def test_default_data_results(benchmark):
     def fetch():
         response = requests.get(f"{HOST}/api/v0/default/results")
         response.raise_for_status()
-        await asyncio.sleep(10)
+        await asyncio.sleep(3)
 
     benchmark(fetch)
 
@@ -28,7 +28,7 @@ def test_default_data_changes(benchmark):
     test_name = response.json()[0]
 
     def fetch(test_name):
-        await asyncio.sleep(10)
+        await asyncio.sleep(3)
         response = requests.get(f"{HOST}/api/v0/default/result/{test_name}/changes")
         response.raise_for_status()
 
