8c4f0ae98aaf90d1ed0b873ffa836e108f985324
Henrik Ingo <henrik@nyrk.io>
2025-10-23 02:06:10 +0300

    Temporary fix to avoid  500
    One of the public/...?notify=1 was untested and broken
    This avoids the 500, but it fails to find the pr and hence
    the change point

--- backend/api/changes.py
+++ backend/api/changes.py
@@ -192,7 +192,9 @@ async def _calc_changes(test_name, user_id=None, pull_request=None, pr_commit=No
         pull_request=pull_request,
     )
     logging.info("DUMP series AND changes JUST TO REVIEW ITS ALL THERE")
-    logging.info(series.results)
+    for r in series.results:
+        logging.info([(m.name, m.value) for m in r.metrics])
+        logging.info(r.attributes)
     logging.info({k: a.to_json() for k, a in changes.items()})
     logging.info(is_cached)
     return series, changes, is_cached
