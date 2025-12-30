eda7bddb9b22c586fc895d97b8810cec77c1241c
Henrik Ingo <henrik@nyrk.io>
2025-12-30 18:42:30 +0200

    Only insert to Mongodb when list is nonempty

--- backend/api/background.py
+++ backend/api/background.py
@@ -52,9 +52,11 @@ async def check_runner_usage():
             await store.add_user_runner_usage(
                 user_id, user_runner_usage, latest_usage_report
             )
-            await store.add_user_runner_usage_raw(
-                user_id, raw_line_items, latest_usage_report
-            )
+            if raw_line_items:
+                await store.add_user_runner_usage_raw(
+                    user_id, raw_line_items, latest_usage_report
+                )
+
     if latest_usage_report:
         logger.info(
             f"We now imported s3 consumption reports up to and including {latest_usage_report}"
