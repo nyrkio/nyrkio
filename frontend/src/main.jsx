2408b148d4df20aecfdb16a65ecbd6bd3993329a
Henrik Ingo <henrik@nyrk.io>
2026-02-06 23:51:30 +0200

    Move GoogleRecaptchaProvider closer to target

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,25 +11,7 @@ posthog.init("phc_pj5OcqAS68hS0aIO0a3lqodk6lRekPEsz3VMdz6o7Z1", {
 
 */
 
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleReCaptchaProvider
-  reCaptchaKey="6LehQ1IsAAAAACQWFomHKj-zBF_cMG91fWzk4nlh"
-  scriptProps={{
-    async: true, // optional, default to false,
-    defer: false, // optional, default to false
-    appendTo: 'head', // optional, default to "head", can be "head" or "body",
-    nonce: undefined // optional, default undefined
-  }}
-  container={{ // optional to render inside custom element
-    element: "recaptchadiv",
-    parameters: {
-      badge: 'inline', // optional, default undefined
-      // theme: 'light', // optional, default undefined
-    }
-  }}
-  >
   <App />
-  </GoogleReCaptchaProvider>
 );
