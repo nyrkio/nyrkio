2d93734cc5e9f0ed989a468449a8dfeab4c58247
MykolaM <mmishchanchuk@golems.team>
2026-05-28 15:38:47 +0300

    Design changes: Update design for 404 page.

--- frontend/src/components/NoMatch.jsx
+++ frontend/src/components/NoMatch.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import { BackButton } from "./BackButton";
 export function NoMatch() {
   return (
     <div className="container text-center justify-content-center p-5">
-      <h2>404: Page Not Found</h2>
+      <h2>
+        <div className="text-primary">404</div>
+        <div className="h3 text-secondary">OOPS...</div>
+      </h2>
+      <p className="my-4">This page not found!</p>
+      <BackButton className="btn btn-primary" />
     </div>
   );
 }
